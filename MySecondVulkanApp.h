b8c573f858aebd992177aac8344dd146f83d8180
NouCake <KevinFischer2010@gmail.com>
2020-03-13 14:12:27 +0100

    VulkanInstance with validation layer + glfw extensions

--- MySecondVulkanApp.h
+++ MySecondVulkanApp.h
@@ -11,6 +11,7 @@ class HelloTriangleApplication {
 public:
 	void run() {
 		initWindow();
+		initVulkan();
 		mainLoop();
 		cleanup();
 	}
@@ -48,6 +49,48 @@ private:
 
 		mWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", nullptr, nullptr);
 
+	}
+
+	//Ripped from: https://github.com/KhronosGroup/Vulkan-Hpp/blob/master/samples/EnableValidationWithCallback/EnableValidationWithCallback.cpp
+	bool checkLayers(std::vector<char const*> const& layers, std::vector<vk::LayerProperties> const& properties) {
+		// return true if all layers are listed in the properties
+		return std::all_of(layers.begin(), layers.end(), [&properties](char const* name) {
+			return std::find_if(properties.begin(), properties.end(), [&name](vk::LayerProperties const& property) { return strcmp(property.layerName, name) == 0; }) != properties.end();
+		});
+	}
+
+	void initVulkan() {
+		createInstance();
+	}
+	void createInstance() {
+
+		// Creatig Vulkan Instance with extensions for glfw & validation layer
+		vk::ApplicationInfo applicationInfo("Vulkan AppName", 1, "", 1, VK_API_VERSION_1_2);
+
+		//getting required extensions from glfw
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		//creating a list of layers
+		std::vector<char const*> instanceLayerNames;
+		//TODO: If debug
+		instanceLayerNames.push_back("VK_LAYER_KHRONOS_validation");
+		std::vector<vk::LayerProperties> instanceLayerProperties = vk::enumerateInstanceLayerProperties();
+
+		if (!checkLayers(instanceLayerNames, instanceLayerProperties)) {
+			std::cout << "Set the environment variable VK_LAYER_PATH to point to the location of your layers" << std::endl;
+			exit(1);
+		}
+
+		vk::InstanceCreateInfo instanceCreateInfo({}, &applicationInfo);
+		instanceCreateInfo.enabledExtensionCount = glfwExtensionCount;
+		instanceCreateInfo.ppEnabledExtensionNames = glfwExtensions;
+		instanceCreateInfo.enabledLayerCount = instanceLayerNames.size();
+		instanceCreateInfo.ppEnabledLayerNames = instanceLayerNames.data();
+
+
+		mInstance = vk::createInstance(instanceCreateInfo);
 	}
 	void mainLoop() {
 		while (!glfwWindowShouldClose(mWindow)) {
